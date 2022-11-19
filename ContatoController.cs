21a7196801820ee976220ac26a759ebcd5235bd6
EDIMIR LUIZ <76841015+luizk9@users.noreply.github.com>
2022-11-19 03:46:12 -0300

    1-arquivos iniciais
    Apenas os arquivos que foram criados ou modificados na 1 etapa, que foram layaut e controle

---
+++ ContatoController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using projContato.Models;
+
+namespace projContato.Controllers;
+
+public class ContatoController : Controller
+{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Criar()
+    {
+        return View();
+    }
+    
+    public IActionResult Editar()
+    {
+        return View();
+    }
+
+    public IActionResult ApagarConfirmacao()
+    {
+        return View();
+    }
+
+
+
+
+
+  
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
